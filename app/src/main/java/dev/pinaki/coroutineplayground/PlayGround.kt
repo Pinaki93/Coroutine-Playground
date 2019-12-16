6c2b66dda6a8b43fbbda8b1107fb5bd5e59463ed
pinaki93 <pinaki.acharya@moneyview.in>
2019-12-16 22:09:02 +0530

    Added blocking vs suspending snippet

---
+++ app/src/main/java/dev/pinaki/coroutineplayground/PlayGround.kt
@@ -0,0 +1,24 @@
+package dev.pinaki.coroutineplayground
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        // Start a coroutine
+        launch(coroutineContext) {
+            println("In start : ${getThreadName()}")
+            delay(200)
+            println("In ended : ${getThreadName()}")
+        }
+
+        run {
+            println("Out start: ${getThreadName()}")
+            delay(300) //blocking
+            println("Out ended: ${getThreadName()}")
+        }
+    }
+}
+
+fun getThreadName() = Thread.currentThread().name
